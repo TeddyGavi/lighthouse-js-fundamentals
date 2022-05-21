2e6fa351b17e89ae1763dfed27e3d8db58207543
Matthew Davis <matc.davis@gmail.com>
2022-05-21 21:26:54 +0000

    looping or something

--- chorus-while.js
+++ chorus-while.js
@@ -1,9 +1,9 @@
-const chorus = "Let's Dance";
+const chorus = "Let's dance!";
 let repeat = 0;
 while (repeat < 10){
   if (repeat === 5){
     console.log("*chanege key*");
-  }
+  }d
   console.log(chorus);
   repeat++;
 }
